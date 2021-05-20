ced53ff73815dd8d18920a530c3eddbced930316
bukanspot <spot@tracert.id>
2021-05-21 00:55:03 +0800

    view buku

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Buku;
+use App\JenisBuku;
 
 class BukuController extends Controller
 {
-    //
+    public function index()
+    {
+        $buku = Buku::get();
+        $jenis = JenisBuku::get();
+
+        return view('buku')
+            ->with(['buku' => $buku])
+            ->with(['jenis' => $jenis])
+        ;
+    }
 }
